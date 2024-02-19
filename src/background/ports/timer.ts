1edb7ef9218670627ae9696b5e14923314fb6a84
Anand R <graceanand99@gmail.com>
2024-02-19 08:24:18 +0530

    Fix break time bug

--- src/background/ports/timer.ts
+++ src/background/ports/timer.ts
@@ -4,24 +4,20 @@ import storage from "~storage"
 
 const handler: PlasmoMessaging.PortHandler = async (req, res) => {
   if (req.body.action === "start") {
-    storage.set("timerStatus", "pomo_running")
+    await storage.set("timerStatus", "pomo_running")
+    await storage.set("timerLeft", 25 * 60)
     return
   } else if (req.body.action === "stop") {
-    storage.set("timerStatus", "stopped")
+    await storage.set("timerStatus", "stopped")
     return
   } else if (req.body.action === "get") {
-    if ((await storage.get("timerStatus")) === "pomo_running") {
-      const timerInterval = setInterval(async () => {
+    const timerInterval = setInterval(async () => {
+      if ((await storage.get("timerStatus")) === "pomo_running") {
         const time = (await storage.get("timerLeft")) as unknown as number
 
         if (!time) {
-          if ((await storage.get("timerStatus")) === "pomo_running") {
-            storage.set("timerStatus", "break_running")
-            await storage.set("timerLeft", 5 * 60)
-          } else {
-            await storage.set("timerStatus", "pomo_running")
-            await storage.set("timerLeft", 25 * 60)
-          }
+          await storage.set("timerStatus", "break_running")
+          await storage.set("timerLeft", 5 * 60)
         } else {
           res.send({
             time: time - 1,
@@ -33,23 +29,26 @@ const handler: PlasmoMessaging.PortHandler = async (req, res) => {
         if ((await storage.get("timerStatus")) === "stopped") {
           clearInterval(timerInterval)
         }
-      }, 1000)
-    } else if ((await storage.get("timerStatus")) === "break_running") {
-      const timerInterval = setInterval(async () => {
+      } else if ((await storage.get("timerStatus")) === "break_running") {
         const time = (await storage.get("timerLeft")) as unknown as number
 
-        res.send({
-          time: time - 1,
-          percentage: (time - 1) / (5 * 60),
-          status: "break_running"
-        })
-        storage.set("timerLeft", time - 1)
+        if (!time) {
+          await storage.set("timerStatus", "pomo_running")
+          await storage.set("timerLeft", 25 * 60)
+        } else {
+          res.send({
+            time: time - 1,
+            percentage: (time - 1) / (5 * 60),
+            status: "break_running"
+          })
+          await storage.set("timerLeft", time - 1)
+        }
 
         if ((await storage.get("timerStatus")) === "stopped") {
           clearInterval(timerInterval)
         }
-      }, 1000)
-    }
+      }
+    }, 1000)
   }
 }
 
